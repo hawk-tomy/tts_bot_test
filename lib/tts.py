3982b231e5aa744ed3f815123f4bb639818db2f2
hawk_tomy <hawk201000211101@gmail.com>
2021-06-14 00:45:50 +0900

    update lib/tts

--- lib/tts.py
+++ lib/tts.py
@@ -1,15 +1,32 @@
+from asyncio import AbstractEventLoop
+from audioop import tostereo
+from concurrent.futures import ThreadPoolExecutor
+from io import BytesIO
+from struct import pack
+
+
+from discord import PCMAudio
+
+
 from .jtalk import JTalk
 
 
 class TTS:
-    def __init__(self):
+    def __init__(self, loop: AbstractEventLoop):
+        self.loop = loop
         self.jtalk = JTalk()
+        self.exec = ThreadPoolExecutor()
+
+    def get_source(self, text: str) -> BytesIO:
+        pcm = self.jtalk.generate_pcm(text)
+        bin_pcm = pack("h"*len(pcm), *pcm)
+        return BytesIO(tostereo(bin_pcm, 2, 1, 1))
 
     def set_voice_setting(
             self,
-            voice: str,
-            speed: float,
-            tone: float
+            voice: str= 'normal',
+            speed: float= 1.0,
+            tone: float= 0
     ):
         #voice: 'normal', 'happy', 'bashful', 'angry', 'sad'
         #speed: 0.5 - 2
@@ -17,3 +34,14 @@ class TTS:
         self.jtalk.set_voice(f'mei_{voice}')
         self.jtalk.set_speed(speed)
         self.jtalk.set_tone(tone)
+
+    async def generate_source(
+            self,
+            text,
+            voice: str= 'normal',
+            speed: float= 1.0,
+            tone: float= 0
+    ):
+        self.set_voice_setting(voice, speed, tone)
+        pcm = await self.loop.run_in_executor(self.exec, self.get_source, text)
+        return PCMAudio(pcm)
